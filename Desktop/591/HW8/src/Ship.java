2fe5d87525618fd343072963812bbd106f929d15
Jess <Jess@192.168.0.6>
2020-12-11 09:12:34 -0500

    updated ship with vars, getters, and setters

--- Desktop/591/HW8/src/Ship.java
+++ Desktop/591/HW8/src/Ship.java
@@ -1,4 +1,43 @@
 
-public class Ship {
-
+public abstract class Ship {
+	
+	private int bowRow;
+	private int bowColumn;
+	protected int length;
+	private boolean horizontal;
+	protected boolean[] hit = new boolean[4];
+	
+	
+	public Ship() {
+	}
+	
+	int getBowRow() {
+		return bowRow;
+	}
+	
+	int getBowColumn() {
+		return bowColumn;
+	}
+	
+	boolean isHorizontal() {
+		return horizontal;
+	}
+	
+	void setBowRow(int row) {
+		this.bowRow = row;
+	}
+	
+	void setBowColumn(int column) {
+		this.bowColumn = column;
+	}
+	
+	void setHorizontal(boolean horizontal) {
+		this.horizontal = horizontal;
+	}
+	
+	abstract int getLength();
+	abstract String getShipType();
+	
+	
+	
 }
