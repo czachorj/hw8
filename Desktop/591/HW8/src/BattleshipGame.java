5c9a5f1d5319c55e60a9aae3f7b17f636f3bb1cf
sengheta <sarah.engheta@gmail.com>
2020-12-12 00:59:33 -0500

    added methods for playing again, also added ifSunk condition

--- Desktop/591/HW8/src/BattleshipGame.java
+++ Desktop/591/HW8/src/BattleshipGame.java
@@ -19,7 +19,7 @@ public class BattleshipGame {
 //		
 //	}
 	
-	void battlshipGameRunner() {
+	void battleshipGameRunner() {
 		Ocean newOcean = new Ocean();
 		int r=0;
 		int c=0;
@@ -29,18 +29,43 @@ public class BattleshipGame {
 		
 		while(!newOcean.isGameOver()) {
 		System.out.println("Enter the coordinate you'd like to attack. Format should be row number then column number. Each an integer between 0 and 9:");
-		r = s.nextInt();
+		r = s.nextInt();                                    //scan input for r (row) and c (col) coordinates from user
 		c = s.nextInt();
 
-		if(newOcean.shootAt(r, c)) {
+		if(newOcean.shootAt(r, c)) {                        //if shootAt is true i.e. if we hit, print hit
 			System.out.println("Hit!");
+			Ship [][] targetShip = newOcean.getShipArray(); //get array of ocean at current state
+			if(targetShip[r][c].isSunk()) {                 //if targetShip at the location hit is sunk, print what type of ship got sunk
+				System.out.println("You just sunk a " + targetShip[r][c].getShipType());
+			}
+			
 		}
 		else {
-			System.out.println("Miss.");
+			System.out.println("Miss.");                    //if shootAt is false, that means it was a miss
 		}
 		
-		System.out.println(newOcean.getShotsFired());
+		System.out.println("Shots Fired: " + newOcean.getShotsFired());    //print out current count of shots
+		newOcean.print();                //print ocean as is
+		}
 		
+
+		System.out.println("You sunk all the ships in " + newOcean.getShotsFired() + " turns.");
+		
+	}
+	
+	/*
+	 * method to determine if player wants to play again
+	 */
+	boolean playAgain() {
+		Scanner s = new Scanner(System.in);
+		String answer;
+		System.out.println("Do you want to play again? (y/n) ");
+		answer = s.next();
+		if(answer.startsWith("y") || answer.startsWith("Y")) {
+			return true;
+		}
+		else {
+			return false;
 		}
 		
 	}
@@ -48,7 +73,12 @@ public class BattleshipGame {
 	
 	public static void main(String[] args) {
 		BattleshipGame newGame = new BattleshipGame();
-		newGame.battlshipGameRunner();
+		newGame.battleshipGameRunner();       //play first game
+		
+		while(newGame.playAgain()) {           //while player wants to keep playing, keep looping and calling battleshipGamerunner
+			newGame.battleshipGameRunner();
+		}
+		
 		
 
 		//right now this just prints the ship placements -- need to fix print method.
