8f6caab04a30965a65c0f3897e26dcfab13408dd
sengheta <sarah.engheta@gmail.com>
2020-12-12 23:20:44 -0500

    starting on printing column labels

--- Desktop/591/HW8/src/Ocean.java
+++ Desktop/591/HW8/src/Ocean.java
@@ -166,8 +166,9 @@ public class Ocean {
 	
 	public void print() {
 		for(int i=0; i<10; i++) {
+			System.out.println("    0 1 2 3 4 5 6 7 8 9 ");
 			for(int j=0; j<10; j++) {
-				if()
+				System.out.println(ships[i][j].toString());   //will print x if hit and s if sunk for ships fired at
 			}
 		}
 	}
