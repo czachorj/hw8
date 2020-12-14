8c59e6eb63777c9786f4c0ab971374c736d8be87
sengheta <sarah.engheta@gmail.com>
2020-12-14 14:23:40 -0500

    created junit shiptest

---
+++ Desktop/591/HW8/src/ShipTest.java
@@ -0,0 +1,72 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShipTest {
+
+	@Test
+	void testGetBowRow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBowColumn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsHorizontal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetBowRow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetBowColumn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetHorizontal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetShipType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsSunk() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testShootAt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testOkToPlaceShipAt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlaceShipAt() {
+		fail("Not yet implemented");
+	}
+
+}
