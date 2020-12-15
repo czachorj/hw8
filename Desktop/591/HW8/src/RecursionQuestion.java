2b6679c90d8d56a591ea01576db13d6f3dddab5b
sengheta <sarah.engheta@gmail.com>
2020-12-15 17:58:37 -0500

    first recursion question done

--- Desktop/591/HW8/src/RecursionQuestion.java
+++ Desktop/591/HW8/src/RecursionQuestion.java
@@ -1,10 +1,34 @@
 //
-//public class RecursionQuestion {
-//
-//	
-//	static String killCommas(String s) {
-//		return s;
-//	}
+public class RecursionQuestion {
+	public static void main(String[] args) {
+		killCommas("sd,f,fsdf,sdf,");
+	}
+
+	static String killCommas(String s) {
+		int commaCount = 0;
+
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i) == ',') {
+				commaCount++;
+			}
+		}
+
+		//base case - no more commas
+		if(commaCount ==0) {
+			System.out.println(s);
+			return s;
+		}
+
+		//recursion case
+		else {
+			int index = s.indexOf(','); //first occurrence of comma
+
+
+			return killCommas(s.substring(0, index) + s.substring(index + 1)); //concatenate the string up to the comma, and everything after the comma
+		}
+
+	}
+}
 //	
 //	
 //	static int sumDigits(int num) {
@@ -17,4 +41,4 @@
 //	}
 //	
 //	
-//}
+
