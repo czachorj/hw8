d6ef9fb4f1cf2af684eec6fe79891666ee60787a
Jess <Jess@192.168.0.6>
2020-12-15 19:42:04 -0500

    Q2 done

--- Desktop/591/HW8/src/RecursionQuestion.java
+++ Desktop/591/HW8/src/RecursionQuestion.java
@@ -28,6 +28,18 @@ public class RecursionQuestion {
 		}
 
 	}
+	
+	
+	static int sumDigits(int num) {
+		
+		if (num == 0) {
+			return 0;
+		}
+		return (num % 10 + sumDigits(num/10));
+		
+	}
+	
+	
 }
 //	
 //	
